7c71d45b47578ee1e9fda7ae1de6e62477124168
mylonjones <mylonndj1234@gmail.com>
2021-08-22 15:10:24 -0700

    set up react and webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.js`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  mode: 'development',
+  devServer: {
+    contentBase: path.join(__dirname, 'client/dist'),
+    compress: true,
+    port: 9000,
+  },
+  module : {
+    rules : [
+      {
+        test : /\.jsx?/,
+        include : SRC_DIR,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+};

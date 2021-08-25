ebd5051c802c36912df4dcdd64a645e6201b56b8
mylonjones <mylonndj1234@gmail.com>
2021-08-25 15:57:08 -0700

    added parallax scrolling

--- client/src/app.js
+++ client/src/app.js
@@ -13,6 +13,14 @@ export default class App extends React.Component {
     return (
       <div>
         <Navbar/>
+        <div className="bodyWrapper">
+          <div className="parallax">
+            <div className="content">
+            lots of wordslots of wordslots of wordslots of wordslots of wordslots of wordslots of wordslots of wordslots of wordslots of wordslots of wordslots of wordslots of wordslots of wordslots of words
+            </div>
+          </div>
+          <div className="regular" style={{height: '1000px', backgroundColor: 'red', width: '100%'}}></div>
+        </div>
       </div>
     )
   }
